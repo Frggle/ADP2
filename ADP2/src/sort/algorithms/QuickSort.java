62883b5048072287cbb352ef635f00b23f753d34
Frggle <marc.kaepke@gmail.com>
2015-11-27 13:26:57 +0100

    JavaDoc fuer QuickSort ergaenzt
    Fehler in ADTArray behoben

--- ADP2/src/sort/algorithms/QuickSort.java
+++ ADP2/src/sort/algorithms/QuickSort.java
@@ -4,212 +4,271 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-
 import sort.AccessCount;
 import sort.MethodPivot;
 import adt.interfaces.AdtArray;
 
 public class QuickSort {
-
-    private static AdtArray _numbers;
-    private static String _pivot;
-    
-    private static int writeAccessCount = 0;
-    private static int readAccessCount = 0;
-    private static List<Long> runtimeInsertionSort = new ArrayList<Long>();
-
-    /**
-     * 
-     * @param ilinks
-     * @param irechts
-     * @return
-     */
-    private static int setPivot(int ilinks, int irechts) {
-        if (MethodPivot.valueOf(_pivot) == MethodPivot.LEFT) {
-            swap(ilinks, irechts);
-        } else if (MethodPivot.valueOf(_pivot) == MethodPivot.RANDOM) {
-            ThreadLocalRandom random = ThreadLocalRandom.current();
-            int i = random.nextInt(ilinks, irechts + 1);
-            swap(i, irechts);
-        } else if (MethodPivot.valueOf(_pivot) == MethodPivot.MEDIANOF3) {
-            List<Integer> list = new ArrayList<Integer>();
-            int links = _numbers.getA(ilinks);
-            list.add(links);
-            int rechts = _numbers.getA(irechts);
-            list.add(rechts);
-            int tmp = (irechts + ilinks) / 2;
-            int mid = _numbers.getA(tmp);
-            list.add(mid);
-            Collections.sort(list);
-            int tmpValue = list.get(1);
-            if (tmpValue == links) {
-                swap(ilinks, irechts);
-            } else if (tmpValue == mid) {
-                swap(tmp, irechts);
-            }
-        } 
-        return _numbers.getA(irechts);
-    }
-    
-    /**
-     * 
-     * @param ilinks
-     * @param irechts
-     * @return
-     */
-    private static int setPivotWithAccessCount(int ilinks, int irechts) {
-        if (MethodPivot.valueOf(_pivot) == MethodPivot.LEFT) {
-            swapWithAccessCount(ilinks, irechts);
-        } else if (MethodPivot.valueOf(_pivot) == MethodPivot.RANDOM) {
-            ThreadLocalRandom random = ThreadLocalRandom.current();
-            int i = random.nextInt(ilinks, irechts + 1);
-            swapWithAccessCount(i, irechts);
-        } else if (MethodPivot.valueOf(_pivot) == MethodPivot.MEDIANOF3) {
-            List<Integer> list = new ArrayList<Integer>();
-            int links = _numbers.getA(ilinks);
-            readAccessCount++;
-            list.add(links);
-            int rechts = _numbers.getA(irechts);
-            readAccessCount++;
-            list.add(rechts);
-            int tmp = (irechts + ilinks) / 2;
-            int mid = _numbers.getA(tmp);
-            readAccessCount++;
-            list.add(mid);
-            Collections.sort(list);
-            int tmpValue = list.get(1);
-            readAccessCount++;
-            if (tmpValue == links) {
-                swapWithAccessCount(ilinks, irechts);
-            } else if (tmpValue == mid) {
-                swapWithAccessCount(tmp, irechts);
-            }
-        } 
-        readAccessCount++;
-        return _numbers.getA(irechts);
-    }
-    
-    /**
-     * 
-     * @param i
-     * @param j
-     */
-    private static void swap(int i, int j) {
-        int temp = _numbers.getA(i);
-        _numbers.setA(i, _numbers.getA(j));
-        _numbers.setA(j, temp);
-    }
-    
-    private static void swapWithAccessCount(int i, int j) {
-        int temp = _numbers.getA(i);
-        readAccessCount++;
-        _numbers.setA(i, _numbers.getA(j));
-        readAccessCount++;
-        writeAccessCount++;
-        _numbers.setA(j, temp);
-        writeAccessCount++;
-    }
-
-    public static void sort(AdtArray numbers, String pivot) {
-        _numbers = numbers;
-        _pivot = pivot;
-
-        quickSort(0, _numbers.lengthA() - 1);
-    }
-
-    private static void quickSort(int ilinks, int irechts) {
-        int size = irechts - ilinks + 1;
-        if (size < 12) {
-            InsertionSort.sort(_numbers, ilinks, irechts);
-        } else {
-            int i = quickSwap(ilinks, irechts);
-            quickSort(ilinks, i - 1);
-            quickSort(i + 1, irechts);
-        }
-    }
-
-    private static int quickSwap(int ilinks, int irechts) {
-        int i = ilinks;
-        int j = irechts - 1;
-        int pivot = setPivot(ilinks, irechts);
-
-        while (i <= j) {
-            while ((_numbers.getA(i) <= pivot) && (i < irechts))
-                i++;
-            while ((ilinks <= j) && (_numbers.getA(j) > pivot))
-                j--;
-            if (i < j)
-                swap(i, j);
-        }
-        swap(i, irechts);
-        return i;
-    }
-    
-    public static long sortWithRuntime(AdtArray numbers, String pivot) {
-        long startTime = System.currentTimeMillis();
-        _numbers = numbers;
-        _pivot = pivot;
-
-        quickSortWithRuntime(0, _numbers.lengthA() - 1);
-        
-        long runtime = System.currentTimeMillis() - startTime;
-        for(long l : runtimeInsertionSort) {
-            runtime -= l;
-        }
-        
-        return runtime;
-    }
-
-    private static void quickSortWithRuntime(int ilinks, int irechts) {
-        int size = irechts - ilinks + 1;
-        if (size < 12) {
-            long startTime = System.currentTimeMillis();
-            InsertionSort.sort(_numbers, ilinks, irechts);
-            runtimeInsertionSort.add(System.currentTimeMillis() - startTime);
-        } else {
-            int i = quickSwap(ilinks, irechts);
-            quickSortWithRuntime(ilinks, i - 1);
-            quickSortWithRuntime(i + 1, irechts);
-        }
-    }
-
-    public static AccessCount sortWithAccessCount(AdtArray numbers, String pivot) {
-        _numbers = numbers;
-        _pivot = pivot;
-
-        quickSortWithAccessCount(0, _numbers.lengthA() - 1);
-        
-        return new AccessCount(readAccessCount, writeAccessCount);
-    }
-
-    private static void quickSortWithAccessCount(int ilinks, int irechts) {
-        int size = irechts - ilinks + 1;
-        if (size < 12) {
-            InsertionSort.sort(_numbers, ilinks, irechts);
-        } else {
-            int i = quickSwapWithAccessCount(ilinks, irechts);
-            quickSortWithAccessCount(ilinks, i - 1);
-            quickSortWithAccessCount(i + 1, irechts);
-        }
-    }
-    
-    private static int quickSwapWithAccessCount(int ilinks, int irechts) {
-        int i = ilinks;
-        int j = irechts - 1;
-        int pivot = setPivotWithAccessCount(ilinks, irechts);
-
-        while (i <= j) {
-            while ((_numbers.getA(i) <= pivot) && (i < irechts))
-                readAccessCount++;
-                i++;
-            while ((ilinks <= j) && (_numbers.getA(j) > pivot))
-                readAccessCount++;
-                j--;
-            if (i < j)
-                swapWithAccessCount(i, j);
-        }
-        swapWithAccessCount(i, irechts);
-        return i;
-    }
-
+	
+	/* destruktives ADTArray und der PIVOT */
+	private static AdtArray _numbers;
+	private static String _pivot;
+	
+	/* Zugriffszaehler */
+	private static int writeAccessCount = 0;
+	private static int readAccessCount = 0;
+	
+	/* List mit den Runtime-Messungen vom InsertionSort */
+	private static List<Long> runtimeInsertionSort = new ArrayList<Long>();
+	
+	/**
+	 * Setzt das Pivot-Element nach rechts (innerhalb des aktuellen Bereichs) und gibt das Pivot-Element zurueck.
+	 * @param ilinks, der StartIndex vom aktuellen Bereich
+	 * @param irechts, der EndIndex vom aktuellen Bereich
+	 * @return das Pivot-Element
+	 */
+	private static int setPivot(int ilinks, int irechts) {
+		if(MethodPivot.valueOf(_pivot) == MethodPivot.LEFT) {
+			swap(ilinks, irechts);
+		} else if(MethodPivot.valueOf(_pivot) == MethodPivot.RANDOM) {
+			ThreadLocalRandom random = ThreadLocalRandom.current();
+			int i = random.nextInt(ilinks, irechts + 1);
+			swap(i, irechts);
+		} else if(MethodPivot.valueOf(_pivot) == MethodPivot.MEDIANOF3) {
+			List<Integer> list = new ArrayList<Integer>();
+			int links = _numbers.getA(ilinks);
+			list.add(links);
+			int rechts = _numbers.getA(irechts);
+			list.add(rechts);
+			int tmp = (irechts + ilinks) / 2;
+			int mid = _numbers.getA(tmp);
+			list.add(mid);
+			Collections.sort(list);
+			int tmpValue = list.get(1);
+			if(tmpValue == links) {
+				swap(ilinks, irechts);
+			} else if(tmpValue == mid) {
+				swap(tmp, irechts);
+			}
+		}
+		return _numbers.getA(irechts);
+	}
+	
+	/**
+	 * Setzt das Pivot-Element nach rechts (innerhalb des aktuellen Bereichs) und gibt das Pivot-Element zurueck.
+	 * Zusaetzlich werden die Lese- und Schreibzugriffe gezaehlt.
+	 * @param ilinks, der StartIndex vom aktuellen Bereich
+	 * @param irechts, der EndIndex vom aktuellen Bereich
+	 * @return das Pivot-Element
+	 */
+	private static int setPivotWithAccessCount(int ilinks, int irechts) {
+		if(MethodPivot.valueOf(_pivot) == MethodPivot.LEFT) {
+			swapWithAccessCount(ilinks, irechts);
+		} else if(MethodPivot.valueOf(_pivot) == MethodPivot.RANDOM) {
+			ThreadLocalRandom random = ThreadLocalRandom.current();
+			int i = random.nextInt(ilinks, irechts + 1);
+			swapWithAccessCount(i, irechts);
+		} else if(MethodPivot.valueOf(_pivot) == MethodPivot.MEDIANOF3) {
+			List<Integer> list = new ArrayList<Integer>();
+			int links = _numbers.getA(ilinks);
+			readAccessCount++;
+			list.add(links);
+			int rechts = _numbers.getA(irechts);
+			readAccessCount++;
+			list.add(rechts);
+			int tmp = (irechts + ilinks) / 2;
+			int mid = _numbers.getA(tmp);
+			readAccessCount++;
+			list.add(mid);
+			Collections.sort(list);
+			int tmpValue = list.get(1);
+			readAccessCount++;
+			if(tmpValue == links) {
+				swapWithAccessCount(ilinks, irechts);
+			} else if(tmpValue == mid) {
+				swapWithAccessCount(tmp, irechts);
+			}
+		}
+		readAccessCount++;
+		return _numbers.getA(irechts);
+	}
+	
+	/**
+	 * Tauscht die beiden Elemente die sich im Index i und j befinden.
+	 * @param i, der eine Index, der mit j getauscht wird
+	 * @param j, der andere Index, der mit i getauscht wird
+	 */
+	private static void swap(int i, int j) {
+		int temp = _numbers.getA(i);
+		_numbers.setA(i, _numbers.getA(j));
+		_numbers.setA(j, temp);
+	}
+	
+	/**
+	 * Tauscht die beiden Elemente die sich im Index i und j befinden. Zusaetzlich werden die Lese- und Schreibzugriffe
+	 * gezaehlt.
+	 * @param i, der eine Index, der mit j getauscht wird
+	 * @param j, der andere Index, der mit i getauscht wird
+	 */
+	private static void swapWithAccessCount(int i, int j) {
+		int temp = _numbers.getA(i);
+		readAccessCount++;
+		_numbers.setA(i, _numbers.getA(j));
+		readAccessCount++;
+		writeAccessCount++;
+		_numbers.setA(j, temp);
+		writeAccessCount++;
+	}
+	
+	/**
+	 * Die Eingangsmethode um das Array via QuickSort aufsteigend zu sortieren. Zur Unterstuetzung kommt InsertionSort
+	 * zum Einsatz.
+	 * @param numbers, das destruktiv zu sortierende Array
+	 * @param pivot, welches Vergleichsobjekt benutzt wird - ENUM {LEFT, RIGHT, MEDIANOF3, RANDOM}
+	 */
+	public static void sort(AdtArray numbers, String pivot) {
+		_numbers = numbers;
+		_pivot = pivot;
+		
+		quickSort(0, _numbers.lengthA() - 1);
+	}
+	
+	/**
+	 * Die Rekursion, in dem ein aktueller Bereich verkleinert oder bei weniger als 12 Elementen sortiert wird.
+	 * @param ilinks, der StartIndex vom aktuellen Bereich
+	 * @param irechts, der EndIndex vom aktuellen Bereich
+	 */
+	private static void quickSort(int ilinks, int irechts) {
+		int size = irechts - ilinks + 1;
+		if(size < 12) {
+			InsertionSort.sort(_numbers, ilinks, irechts);
+		} else {
+			int i = quickSwap(ilinks, irechts);
+			quickSort(ilinks, i - 1);
+			quickSort(i + 1, irechts);
+		}
+	}
+	
+	/**
+	 * Nimmt eine erste "Sortierung" vor. Groessere Elemente als Pivot kommen nach rechts und kleinere nach links.
+	 * @param ilinks, der StartIndex vom aktuellen Bereich
+	 * @param irechts, der EndIndex vom aktuellen Bereich
+	 * @return der Index vom Pivot
+	 */
+	private static int quickSwap(int ilinks, int irechts) {
+		int i = ilinks;
+		int j = irechts - 1;
+		int pivot = setPivot(ilinks, irechts);
+		
+		while(i <= j) {
+			while((_numbers.getA(i) <= pivot) && (i < irechts))
+				i++;
+			while((ilinks <= j) && (_numbers.getA(j) > pivot))
+				j--;
+			if(i < j)
+				swap(i, j);
+		}
+		swap(i, irechts);
+		return i;
+	}
+	
+	/**
+	 * Die Eingangsmethode um das Array via QuickSort aufsteigend zu sortieren. Zur Unterstuetzung kommt InsertionSort
+	 * zum Einsatz. Zusaetzlich wird die Laufzeit gemessen, der InsertionSort Anteil wird nicht beachtet.
+	 * @param numbers, das destruktiv zu sortierende Array
+	 * @param pivot, welches Vergleichsobjekt benutzt wird - ENUM {LEFT, RIGHT, MEDIANOF3, RANDOM}
+	 * @return die gemessene Laufzeit, exklusiv InsertionSort-Anteil, in Millisec
+	 */
+	public static long sortWithRuntime(AdtArray numbers, String pivot) {
+		long startTime = System.currentTimeMillis();
+		_numbers = numbers;
+		_pivot = pivot;
+		
+		quickSortWithRuntime(0, _numbers.lengthA() - 1);
+		
+		long runtime = System.currentTimeMillis() - startTime;
+		for(long l : runtimeInsertionSort) {
+			runtime -= l;
+		}
+		
+		return runtime;
+	}
+	
+	/**
+	 * Die Rekursion, in dem ein aktueller Bereich verkleinert oder bei weniger als 12 Elementen sortiert wird.
+	 * Zusaetzlich wird die Laufzeit gemessen, der InsertionSort Anteil wird nicht beachtet.
+	 * @param ilinks, der StartIndex vom aktuellen Bereich
+	 * @param irechts, der EndIndex vom aktuellen Bereich
+	 */
+	private static void quickSortWithRuntime(int ilinks, int irechts) {
+		int size = irechts - ilinks + 1;
+		if(size < 12) {
+			long startTime = System.currentTimeMillis();
+			InsertionSort.sort(_numbers, ilinks, irechts);
+			runtimeInsertionSort.add(System.currentTimeMillis() - startTime);
+		} else {
+			int i = quickSwap(ilinks, irechts);
+			quickSortWithRuntime(ilinks, i - 1);
+			quickSortWithRuntime(i + 1, irechts);
+		}
+	}
+	
+	/**
+	 * Die Eingangsmethode um das Array via QuickSort aufsteigend zu sortieren. Zur Unterstuetzung kommt InsertionSort
+	 * zum Einsatz. Zusaetzlich werden die Lese- und Schreibzugriffe gezaehlt.
+	 * @param numbers, das destruktiv zu sortierende Array
+	 * @param pivot, welches Vergleichsobjekt benutzt wird - ENUM {LEFT, RIGHT, MEDIANOF3, RANDOM}
+	 * @return ein AccessCount-Objekt welche die Zugriffe haelt
+	 */
+	public static AccessCount sortWithAccessCount(AdtArray numbers, String pivot) {
+		_numbers = numbers;
+		_pivot = pivot;
+		
+		quickSortWithAccessCount(0, _numbers.lengthA() - 1);
+		
+		return new AccessCount(readAccessCount, writeAccessCount);
+	}
+	
+	/**
+	 * Die Rekursion, in dem ein aktueller Bereich verkleinert oder bei weniger als 12 Elementen sortiert wird.
+	 * Zusaetzlich werden die Lese- und Schreibzugriffe gezaehlt.
+	 * @param ilinks, der StartIndex vom aktuellen Bereich
+	 * @param irechts, der EndIndex vom aktuellen Bereich
+	 */
+	private static void quickSortWithAccessCount(int ilinks, int irechts) {
+		int size = irechts - ilinks + 1;
+		if(size < 12) {
+			InsertionSort.sort(_numbers, ilinks, irechts);
+		} else {
+			int i = quickSwapWithAccessCount(ilinks, irechts);
+			quickSortWithAccessCount(ilinks, i - 1);
+			quickSortWithAccessCount(i + 1, irechts);
+		}
+	}
+	
+	/**
+	 * Nimmt eine erste "Sortierung" vor. Groessere Elemente als Pivot kommen nach rechts und kleinere nach links.
+	 * Zusaetzlich werden die Lese- und Schreibzugriffe gezaehlt.
+	 * @param ilinks, der StartIndex vom aktuellen Bereich
+	 * @param irechts, der EndIndex vom aktuellen Bereich
+	 * @return der Index vom Pivot
+	 */
+	private static int quickSwapWithAccessCount(int ilinks, int irechts) {
+		int i = ilinks;
+		int j = irechts - 1;
+		int pivot = setPivotWithAccessCount(ilinks, irechts);
+		
+		while(i <= j) {
+			while((_numbers.getA(i) <= pivot) && (i < irechts))
+				readAccessCount++;
+			i++;
+			while((ilinks <= j) && (_numbers.getA(j) > pivot))
+				readAccessCount++;
+			j--;
+			if(i < j)
+				swapWithAccessCount(i, j);
+		}
+		swapWithAccessCount(i, irechts);
+		return i;
+	}
 }
--- ADP2/src/tests/QuickSortTests.java
+++ ADP2/src/tests/QuickSortTests.java
@@ -1,175 +1,114 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
-
+import sort.MethodPivot;
 import sort.NumGenerator;
-import sort.algorithms.QuickSort;
 import sort.algorithms.QuickSort;
 import adt.implementations.AdtArrayImpl;
 import adt.interfaces.AdtArray;
 
 public class QuickSortTests {
-    /**
-     * Testet 70 identische Elemente
-     */
-    @Test
-    public void testAllEqualsANDRandom() {
-        AdtArray inArray = NumGenerator.readNum("testAllEquals_70");
-        
-        AdtArray tmpArray = AdtArrayImpl.initA();
-        for(int i = 0; i < inArray.lengthA(); i++) {
-        	tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
-        }
-        
-        QuickSort.sort(inArray, "RANDOM");
-        
-        assertEquals(inArray, tmpArray);
-    }
-    
-    /**
-     * Testet 70 identische Elemente
-     */
-    @Test
-    public void testAllEqualsANDMedianOf3() {
-        AdtArray inArray = NumGenerator.readNum("testAllEquals_70");
-        
-        AdtArray tmpArray = AdtArrayImpl.initA();
-        for(int i = 0; i < inArray.lengthA(); i++) {
-        	tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
-        }
-        
-        QuickSort.sort(inArray, "MEDIANOF3");
-        
-        assertEquals(inArray, tmpArray);
-    }
-    
-    /**
-     * Ein aufsteigend sortiertes Array
-     */
-    @Test
-    public void testAlreadySortedANDRandom() {
-        AdtArray inArray = NumGenerator.readNum("testAlreadySorted_30");
-        
-        AdtArray tmpArray = AdtArrayImpl.initA();
-        for(int i = 0; i < inArray.lengthA(); i++) {
-        	tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
-        }
-        
-        QuickSort.sort(inArray, "RANDOM");
-        
-        assertEquals(inArray, tmpArray);
-    }
-    
-    /**
-     * Ein aufsteigend sortiertes Array
-     */
-    @Test
-    public void testAlreadySortedANDMedianOf3() {
-        AdtArray inArray = NumGenerator.readNum("testAlreadySorted_30");
-        
-        AdtArray tmpArray = AdtArrayImpl.initA();
-        for(int i = 0; i < inArray.lengthA(); i++) {
-        	tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
-        }
-        
-        QuickSort.sort(inArray, "MEDIANOF3");
-        
-        assertEquals(inArray, tmpArray);
-    }
-    
-    @Test
-    public void test() {
-    	NumGenerator.sortNum("test", 30);
-    	
-    	AdtArray array = NumGenerator.readNum("test");
-//    	QuickSort_new.sort(array, "MEDIANOF3");
-//    	QuickSort_new.sort(array, "RANDOM");
-    	QuickSort.sort(array, "LEFT");
-//    	QuickSort_new.sort(array, "RIGHT");
-    	for(int i = 0; i < array.lengthA(); i++) {
-    	    System.err.print(array.getA(i) + " - ");
-    	}
-    	System.err.println();
-    }
-    
-    /**
-     * Ein leeres Array
-     */
-    @Test
-    public void testEmptyANDRandom() {
-        AdtArray inArray = NumGenerator.readNum("testEmpty_0");
-        
-        AdtArray tmpArray = AdtArrayImpl.initA();
-        for(int i = 0; i < inArray.lengthA(); i++) {
-        	tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
-        }
-        
-        QuickSort.sort(inArray, "RANDOM");
-        
-        assertEquals(inArray, tmpArray);
-    }
-    
-    /**
-     * Ein leeres Array
-     */
-    @Test
-    public void testEmptyANDMedianOf3() {
-        AdtArray inArray = NumGenerator.readNum("testEmpty_0");
+	
+	/**
+	 * Testet 100 zufaellige Elemente
+	 */
+	@Test
+	public void test100() {
+		for(MethodPivot pivot : MethodPivot.values()) {
+			AdtArray inArray = NumGenerator.readNum("test100");
 
-        AdtArray tmpArray = AdtArrayImpl.initA();
-        for(int i = 0; i < inArray.lengthA(); i++) {
-        	tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
-        }
-        
-        QuickSort.sort(inArray, "MEDIANOF3");
-        
-        assertEquals(inArray, tmpArray);
-    }
-    
-    /**
-     * Ein absteigend sortiertes Array
-     */
-    @Test
-    public void testAlreadySortedDescANDRandom() {
-        AdtArray inArray = NumGenerator.readNum("testAlreadySortedDesc_30");
-        
-        AdtArray tmpArray = AdtArrayImpl.initA();
-        for(int i = 0; i < inArray.lengthA(); i++) {
-        	tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
-        }
-        
-        QuickSort.sort(inArray, "RANDOM");
-        
-        AdtArray reverseInArray = AdtArrayImpl.initA();
-        for(int i = 0; i < sortArray.lengthA(); i++) {
-        	reverseInArray.setA(sortArray.lengthA() - 1 - i, sortArray.getA(i));
-        }
-        
-        assertEquals(tmpArray, reverseInArray);
-    }
+			AdtArray tmpArray = AdtArrayImpl.initA();
+			for(int i = 0; i < inArray.lengthA(); i++) {
+				tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
+			}
+			
+			QuickSort.sort(inArray, pivot.toString());
+			
+			assertEquals(inArray, tmpArray);
+		}
+	}
 
-    /**
-     * Ein absteigend sortiertes Array
-     */
-    @Test
-    public void testAlreadySortedDescANDMedianOf3() {
-        AdtArray inArray = NumGenerator.readNum("testAlreadySortedDesc_30");
-        
-        AdtArray tmpArray = AdtArrayImpl.initA();
-        for(int i = 0; i < inArray.lengthA(); i++) {
-        	tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
-        }
-        
-        AdtArray sortArray = QuickSort.sort(inArray, "MEDIANOF3");
-        
-        AdtArray reverseInArray = AdtArrayImpl.initA();
-        for(int i = 0; i < sortArray.lengthA(); i++) {
-        	reverseInArray.setA(sortArray.lengthA() - 1 - i, sortArray.getA(i));
-        }
-        
-        assertEquals(tmpArray, reverseInArray);
-    }
+	
+	
+	/**
+	 * Testet 70 identische Elemente
+	 */
+	@Test
+	public void testAllEquals() {
+		for(MethodPivot pivot : MethodPivot.values()) {
+			AdtArray inArray = NumGenerator.readNum("testAllEquals_70");
 
+			AdtArray tmpArray = AdtArrayImpl.initA();
+			for(int i = 0; i < inArray.lengthA(); i++) {
+				tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
+			}
+			
+			QuickSort.sort(inArray, pivot.toString());
+			
+			assertEquals(inArray, tmpArray);
+		}
+	}
+	
+	/**
+	 * Ein aufsteigend sortiertes Array
+	 */
+	@Test
+	public void testAlreadySortedASC() {
+		for(MethodPivot pivot : MethodPivot.values()) {
+			AdtArray inArray = NumGenerator.readNum("testAlreadySorted_30");
+			
+			AdtArray tmpArray = AdtArrayImpl.initA();
+			for(int i = 0; i < inArray.lengthA(); i++) {
+				tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
+			}
+			
+			QuickSort.sort(inArray, pivot.toString());
+			
+			assertEquals(inArray, tmpArray);
+		}
+	}
+	
+	/**
+	 * Ein leeres Array
+	 */
+	@Test
+	public void testEmpty() {
+		for(MethodPivot pivot : MethodPivot.values()) {
+			AdtArray inArray = NumGenerator.readNum("testEmpty_0");
+			
+			AdtArray tmpArray = AdtArrayImpl.initA();
+			for(int i = 0; i < inArray.lengthA(); i++) {
+				tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
+			}
+			
+			QuickSort.sort(inArray, pivot.toString());
+			
+			assertEquals(inArray, tmpArray);
+		}
+	}
+	
+	/**
+	 * Ein absteigend sortiertes Array
+	 */
+	@Test
+	public void testAlreadySortedDesc() {
+		for(MethodPivot pivot : MethodPivot.values()) {
+			AdtArray inArray = NumGenerator.readNum("testAlreadySortedDesc_30");
+			
+			AdtArray tmpArray = AdtArrayImpl.initA();
+			for(int i = 0; i < inArray.lengthA(); i++) {
+				tmpArray.setA(tmpArray.lengthA(), inArray.getA(i));
+			}
+			
+			QuickSort.sort(inArray, pivot.toString());
+			
+			AdtArray reverseInArray = AdtArrayImpl.initA();
+			for(int i = 0; i < inArray.lengthA(); i++) {
+				reverseInArray.setA(inArray.lengthA() - 1 - i, inArray.getA(i));
+			}
+			
+			assertEquals(tmpArray, reverseInArray);
+		}
+	}
 }
